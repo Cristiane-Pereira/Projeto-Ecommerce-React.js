ad36cf26bd3d38b965f58438003b16d392891575
Cristiane Pereira da Silva <72608626+Cristiane-Pereira@users.noreply.github.com>
2020-12-04 07:34:00 -0300

    Update JOIN1.php

--- src/Join/JOIN1.php
+++ src/Join/JOIN1.php
@@ -13,10 +13,10 @@ if(!$conn){
 }
 
 
-// SELECT * FROM tabela_cliente JOIN tabela_vendas ON tabela_cliente.Id_cliente = tabela_vendas.Id_cliente;   retorna cliente e oque ele comprou...
-//SELECT * FROM tabela_cliente JOIN tabela_vendas ON tabela_cliente.Id_cliente = tabela_vendas.Id_cliente JOIN tabela_produtos ON tabela_vendas.Id_produto = tabela_produtos.Id_produto;
-//fara a ligaçao das 3 tabelas com clienres,vendas e produtos....... 
-//Consome Informaçoes de chaves PrimariaS e EstrangeiraS, atravez dA Relacão e o uso do Join do meu BD.
+/* SELECT * FROM tabela_cliente JOIN tabela_vendas ON tabela_cliente.Id_cliente = tabela_vendas.Id_cliente;   retorna cliente e oque ele comprou...
+   SELECT * FROM tabela_cliente JOIN tabela_vendas ON tabela_cliente.Id_cliente = tabela_vendas.Id_cliente JOIN tabela_produtos ON tabela_vendas.Id_produto = tabela_produtos.Id_produto;
+   Fara a ligaçao das 3 tabelas com clienres,vendas e produtos....... 
+   Consome Informaçoes de chaves PrimariaS e EstrangeiraS, atravez dA Relacão e o uso do Join do meu BD.*/
 
 
 
